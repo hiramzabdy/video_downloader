8fef1a98260cfc07c1de9e98639e15231469e3d0
Hiram Zabdy <hiramzabdy@hotmail.com>
2025-02-16 19:43:44 -0600

    sort by codec type

--- src/core/qualities_download.py
+++ src/core/qualities_download.py
@@ -26,17 +26,34 @@ def download_video_and_audio(url: str, output_folder: str = "downloads"):
 
         # Mostrar opciones de formato disponibles
         print("Selecciona la calidad del video que deseas descargar:")
-        for i, fmt in enumerate(formats):
+
+        # Filtrar los formatos en orden específico (avc1, vp9, av01)
+        sorted_formats = []
+        for fmt in formats:
+            if 'vcodec' in fmt and fmt['vcodec'] != 'none':
+                vcodec = fmt.get('vcodec')
+                if vcodec.startswith('avc1'):
+                    sorted_formats.append((fmt, 1))  # Ordenamiento por avc1
+                elif vcodec.startswith('vp9'):
+                    sorted_formats.append((fmt, 2))  # Ordenamiento por vp9
+                elif vcodec.startswith('av01'):
+                    sorted_formats.append((fmt, 3))  # Ordenamiento por av01
+
+        # Ordenar los formatos según el índice del ordenamiento (1, 2, 3)
+        sorted_formats.sort(key=lambda x: x[1])
+
+        for i, (fmt, order) in enumerate(sorted_formats):
             itag = str(fmt['format_id'])
             ext = fmt['ext']
             res = fmt.get('resolution', '').replace('+', 'x')
             fps = fmt.get('fps', '')
             size = fmt.get('filesize')
-            size_mib = round(size / (2**20), 2) if isinstance(size, (int, float)) and size >= 0 else "Unknown"
-            if fmt.get('vcodec') != 'none':
-                format_desc = f"{res} {fps} FPS, Size: {size_mib} MiB, video codec: {fmt.get('vcodec')} (format id: {itag})"
+            size_mib = round(size / (2**20), 2) if isinstance(size, (int, float)) and size >= 0 else None
+            if fmt.get('vcodec') != 'none' and isinstance(size_mib, (float)):
+                format_desc = f"{res} {fps} FPS, Size: {size_mib} MiB, video codec: {fmt.get('vcodec')}, Extension: {ext}."
                 print(f"[{i+1}] - {format_desc}")
                 video_format_options[i + 1] = itag
+
 
         while True:
             try:
